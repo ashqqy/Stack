b1c9d3c3c733a08bf020461cea3a998c861a2b18
ashqqy <Artiom.shaff07@gmail.com>
2024-10-06 00:00:50 +0300

    Версия 3
    Функции реаллокации теперь универсальные и не привязаны к стеку. Во все функции добавлен return ошибок. Функция StackResize и StackErrDescr

--- include/MyAllocation.h
+++ include/MyAllocation.h
@@ -1,9 +1,9 @@
 #ifndef MY_ALLOCATION
 #define MY_ALLOCATION
 
-#include <cstring>
+#include <string.h>
 
-void* MyCalloc (size_t n_elems, size_t size_elems, int memset_elem);
-void* MyRecalloc (void* memory, size_t n_elements, size_t size_elements, size_t previous_n_elements = 0, int memset_elem = 0);
+void* MyCalloc (size_t n_elems, size_t size_elems, void* poison);
+void* MyRecalloc (void* memory, size_t n_elements, size_t size_elements, size_t previous_n_elements, void* poison);
 
 #endif //MY_ALLOCATION
